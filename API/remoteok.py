37d300193f969781efe4d5a5c67ea703302301ce
yon11b <qorgkdus12@naver.com>
2023-06-29 20:43:03 +0900

    [ADD] all api(remoteok) tuples insert in DB

--- API/remoteok.py
+++ API/remoteok.py
@@ -8,55 +8,55 @@ from random import choice
 import datetime
 
 
-def push_data_to_mysql(data):
+def push_data_to_mysql(datas):
     
-    print(data[1]['slug'])
-    
-    RECRUIT = data[1]
-    print(RECRUIT['slug'])
-    print(RECRUIT['date'])
-    print(RECRUIT['company'])
-    print(RECRUIT['company_logo'])
-    print(RECRUIT['position'])
-    print(RECRUIT['tags'])
-    print(RECRUIT['description'])
-    print(RECRUIT['location'])
-    print(RECRUIT['salary_max'])
-    print(RECRUIT['url'])
-    print(RECRUIT['apply_url'])
-    
-    # string 타입의 date를 UNIX 시간으로 변환
-    datetime_obj = datetime.datetime.strptime(RECRUIT['date'], '%Y-%m-%dT%H:%M:%S%z')    
-    unix_time = int(datetime_obj.timestamp())
-    print(unix_time)
-    
-    # tags를 하나의 string으로 변환
-    tags_string = ' '.join(RECRUIT['tags'])
-    
-    print("======================================================")
-    con = pymysql.connect(
-        host='database-1.cgy1torosydx.us-east-1.rds.amazonaws.com',
-        port=3306,
-        user='admin',
-        password='swever123!!',
+    for data in datas:
+        if '_______RATE_LIMIT_INFO_______' in data:
+            print("first json value..............")
+            continue
+        print(data['slug'])
+        print(data['date'])
+        print(data['company'])
+        print(data['company_logo'])
+        print(data['position'])
+        print(data['tags'])
+        print(data['description'])
+        print(data['location'])
+        print(data['salary_max'])
+        print(data['url'])
+        print(data['apply_url'])
+        
+        # string 타입의 date를 UNIX 시간으로 변환
+        datetime_obj = datetime.datetime.strptime(data['date'], '%Y-%m-%dT%H:%M:%S%z')    
+        unix_time = int(datetime_obj.timestamp())
+        print(unix_time)
+        
+        # tags를 하나의 string으로 변환
+        tag_string = '[' + ', '.join(f'"{x}"' for x in data['tags']) + ']'
+        
+        con = pymysql.connect(
+            host='database-1.cgy1torosydx.us-east-1.rds.amazonaws.com',
+            port=3306,
+            user='admin',
+            password='swever123!!',
 
-        charset='utf8',
-        db='SWEVER'
-        )
-    cur = con.cursor()
-    print('===================')
-    cur.execute("INSERT INTO RECRUIT_POST(\
-        NATION_ID, COMPANY_NAME, DESCRIPTION_TITLE, DESCRIPTION_CONTENT, COMPANY_APPLY_LINK, POSTED_DATE,\
-        IS_VISA_SPONSORED, IS_REMOTED, COMPANY_LOGO, SALARY, CONTRACT_FORM, COMPANY_PAGE_LINK,\
-        WRITER, ORIGIN, TAG, LOCATION, IS_DEV\
-        )\
-        VALUES(%s,%s,%s,%s,%s,%s,\
-                %s,%s,%s,%s,%s,%s,\
-                %s,%s,%s,%s,%s)",
-        [0,RECRUIT['company'],RECRUIT['slug'],RECRUIT['description'],RECRUIT['apply_url'], unix_time,\
-        '0', '1', RECRUIT['logo'], RECRUIT['salary_max'],RECRUIT['position'],RECRUIT['url'],\
-        'Swever','remoteok.com', tags_string, RECRUIT['location'],'1'])
-    con.commit()
+            charset='utf8',
+            db='SWEVER'
+            )
+        cur = con.cursor()
+        cur.execute("INSERT INTO RECRUIT_POST(\
+            NATION_ID, COMPANY_NAME, DESCRIPTION_TITLE, DESCRIPTION_CONTENT, COMPANY_APPLY_LINK, POSTED_DATE,\
+            IS_VISA_SPONSORED, IS_REMOTED, COMPANY_LOGO, SALARY, CONTRACT_FORM, COMPANY_PAGE_LINK,\
+            WRITER, ORIGIN, TAG, LOCATION, IS_DEV\
+            )\
+            VALUES(%s,%s,%s,%s,%s,%s,\
+                    %s,%s,%s,%s,%s,%s,\
+                    %s,%s,%s,%s,%s)",
+            [0,data['company'],data['slug'],data['description'],data['apply_url'], unix_time,\
+            '0', '1', data['logo'], data['salary_max'],data['position'],data['url'],\
+            'Swever','remoteok.com', tag_string, data['location'],'1'])
+        con.commit()
+        print("ONE TUPLE IS INSERTED!!>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
 
 def get_proxies():
